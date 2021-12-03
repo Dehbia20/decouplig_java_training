349924701d75065230f1a071f17e62a39b0843e7
Dehbia20 <dehbia.djebarrii@gmail.com>
2021-12-03 14:20:07 +0100

    Creation d'un utilisateur robot

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -3,6 +3,12 @@ package fr.lernejo.guessgame;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
 public class Simulation {
     private final Logger logger = LoggerFactory.getLogger("simulation");
     private final Player player;  //TODO add variable type
@@ -35,10 +41,22 @@ public class Simulation {
             return false;
         }
     }
-    public void loopUntilPlayerSucceed() {
-        //TODO implement me$
-        while (nextRound() == false){
-            nextRound();
-    }
+    public void loopUntilPlayerSucceed(long max) {
+        logger.log("Debut de la partie");
+        boolean test=nextRound();
+        int i=0;
+        long Start=System.currentTimeMillis();
+        long End =0;
+        while(!test && i<max )
+        {
+            test=nextRound();
+            i++;
+        }
+        End=System.currentTimeMillis();
+        long TimeTotal = End-Start;
+        Timestamp timestamp=new Timestamp(TimeTotal);
+        logger.log("Temps : "+new SimpleDateFormat("mm:ss.SSS").format(timestamp));
+
+
     }
 }
